2f553361b3bd9979ce5bc195913ae92ffa3b2a4e
liumeng1201 <liumeng1201@gmail.com>
2014-01-16 15:37:28 +0800

    将对PDF文档的查看转移到Fragment中以方便移植。

--- src/com/lm/pdfviewer/MainActivity.java
+++ src/com/lm/pdfviewer/MainActivity.java
@@ -2,10 +2,9 @@ package com.lm.pdfviewer;
 
 import java.io.File;
 
-import org.vudroid.pdfdroid.PdfViewerActivity;
-
 import android.app.Activity;
-import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -14,11 +13,20 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Intent intent = new Intent(MainActivity.this, PdfViewerActivity.class);
+		setContentView(R.layout.activity_main);
 		File file = new File("/sdcard/aa.pdf");
 		Uri uri = Uri.fromFile(file);
-		intent.setData(uri);
-		startActivity(intent);
+//		Intent intent = new Intent(MainActivity.this, PdfViewerActivity.class);
+//		intent.setData(uri);
+//		startActivity(intent);
+		String filename = uri.getLastPathSegment();
+		Fragment fragment = new PdfViewerFragment();
+		Bundle bundle = new Bundle();
+		bundle.putString("filepath", "/sdcard/aa.pdf");
+		fragment.setArguments(bundle);
+		
+		FragmentManager fm = getFragmentManager();
+		fm.beginTransaction().replace(R.id.fragment_container, fragment).commit();
 	}
 
 }
---
+++ src/com/lm/pdfviewer/PdfViewerFragment.java
@@ -0,0 +1,79 @@
+package com.lm.pdfviewer;
+
+import java.io.File;
+
+import org.vudroid.pdfdroid.codec.PdfContext;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.lm.pdfviewer.models.CurrentPageModel;
+import com.lm.pdfviewer.models.DecodingProgressModel;
+import com.lm.pdfviewer.models.ZoomModel;
+
+public class PdfViewerFragment extends Fragment {
+	private Context context;
+	private String filepath;
+	private DecodeService decodeService;
+	private DocumentView documentView;
+	private CurrentPageModel currentPageModel;
+	
+	public PdfViewerFragment() { }
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		context = getActivity().getApplicationContext();
+		Bundle bundle = this.getArguments();
+		if (bundle != null) {
+			filepath = bundle.getString("filepath");
+		}
+		
+		initDecodeService();
+		final ZoomModel zoomModel = new ZoomModel();
+		final DecodingProgressModel progressModel = new DecodingProgressModel();
+		currentPageModel = new CurrentPageModel();
+		documentView = new DocumentView(context, zoomModel, progressModel, currentPageModel);
+		zoomModel.addEventListener(documentView);
+		documentView.setLayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
+		decodeService.setContentResolver(context.getContentResolver());
+		decodeService.setContainerView(documentView);
+		documentView.setDecodeService(decodeService);
+		decodeService.open(Uri.fromFile(new File(filepath)));
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		FrameLayout frameLayout = new FrameLayout(context);
+		frameLayout.addView(documentView);
+		documentView.showDocument();
+		return frameLayout;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		decodeService.recycle();
+		decodeService = null;
+	}
+
+	private void initDecodeService() {
+		if (decodeService == null) {
+			decodeService = createDecodeService();
+		}
+	}
+	
+	private DecodeService createDecodeService() {
+		return new DecodeServiceBase(new PdfContext());
+	}
+}
